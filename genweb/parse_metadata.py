cca29a1ba46f152c298fe4778a94686852c198ba
Marc Page <MarcAllenPage@gmail.com>
2024-08-06 12:14:44 -0700

    Remove hard-coded paths in parse_metadata.py

--- genweb/parse_metadata.py
+++ genweb/parse_metadata.py
@@ -5,7 +5,11 @@
 from xml.etree.ElementTree import parse, ParseError
 from os import walk
 from os.path import join, splitext
+from platform import system
+from sys import exit as return_code
+
 from yaml import dump
+from devopsdriver.settings import Settings
 
 
 def read_xml(path: str) -> dict:
@@ -35,8 +39,20 @@ def read_xml(path: str) -> dict:
 def main() -> None:
     """searches for xml files and merges into yml file"""
     metadata = {}
+    settings = Settings(__file__)
 
-    for root, _, files in walk("/home/pagerk/metadata/xml"):
+    if "xmldir" not in settings:
+        print("ERROR: Please make sure you add xmldir, srcdir, and finalyaml to:")
+        print(
+            "\t"
+            + Settings.PREF_DIR.get(
+                system(), Settings.PREF_DIR[Settings.DEFAULT_PREF_DIR]
+            )
+            + "/parse_metadata.yml"
+        )
+        return_code(1)
+
+    for root, _, files in walk(settings["xmldir"]):
         for file in [f for f in files if splitext(f)[1].lower() == ".xml"]:
             try:
                 metadata[splitext(file)[0]] = read_xml(join(root, file))
@@ -44,7 +60,12 @@ def main() -> None:
             except (ParseError, IndexError, AssertionError) as error:
                 print(join(root, file))
                 print(error)
-    with open("metadata.yml", "w", encoding="utf-8") as file:
+
+    for entry in metadata.values():
+        if entry["type"] == "inline":
+            pass
+
+    with open(settings["finalyaml"], "w", encoding="utf-8") as file:
         dump(metadata, file)
 
 
