c926bf713c1ff78ffb8d625aa0e6d6dd2dc3ea21
Marc Page <MarcAllenPage@gmail.com>
2024-08-06 12:26:15 -0700

    adding more test coverage

--- tests/test_people.py
+++ tests/test_people.py
@@ -10,6 +10,28 @@ from datetime import date
 from genweb.people import People
 
 
+def test_dict_operators() -> None:
+    gedcom_list = [
+        SimpleNamespace(
+            id="1",
+            given="John Smith",
+            surname="Doe",
+            gender="M",
+            birthdate=date(1973, 6, 30),
+            parents=set(),
+            spouses=set(),
+            children=set(),
+        ),
+    ]
+
+    gedcom = {p.id: p for p in gedcom_list}
+    assert len(gedcom) == len(gedcom_list)
+    people = People(gedcom)
+    assert "DoeJohnS1973-" in repr(people), repr(people)
+    assert people.has_key("DoeJohnS1973-"), people
+    assert "DoeJohnS1973-" in people.keys(), people
+
+
 def test_middle_initial() -> None:
     gedcom_list = [
         SimpleNamespace(
@@ -23,14 +45,15 @@ def test_middle_initial() -> None:
             children=set(),
         ),
     ]
-    
+
     gedcom = {p.id: p for p in gedcom_list}
     assert len(gedcom) == len(gedcom_list)
     people = People(gedcom)
     assert len(people) == 1, people
     assert "DoeJohnS1973-" in people, people
     assert people["DoeJohnS1973-"].given == "John Smith", people["DoeJohnS1973-"].given
-    
+
+
 def test_basic() -> None:
     gedcom_list = [
         SimpleNamespace(
@@ -84,10 +107,15 @@ def test_basic() -> None:
     assert "DoeJames2004SmithSally1971" in people, people
     assert people["DoeJohn1973-"].given == "John", people["DoeJohn1973-"]
     assert people["SmithSally1971-"].given == "Sally", people["SmithSally1971-"]
-    assert people["DoeJames2004SmithSally1971"].given == "James", people["DoeJames2004SmithSally1971"]
-    assert people["DoeJane2007SmithSally1971"].given == "Jane", people["DoeJane2007SmithSally1971"]
+    assert people["DoeJames2004SmithSally1971"].given == "James", people[
+        "DoeJames2004SmithSally1971"
+    ]
+    assert people["DoeJane2007SmithSally1971"].given == "Jane", people[
+        "DoeJane2007SmithSally1971"
+    ]
 
 
 if __name__ == "__main__":
+    test_dict_operators()
     test_basic()
     test_middle_initial()
