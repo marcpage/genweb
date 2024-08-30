0e7600aa50a7acfc9cebd2862e00eb7d953a3b51
Marc Page <MarcAllenPage@gmail.com>
2024-08-30 07:45:50 -0500

    Add ability to get file hashes from inventory

--- tests/test_inventory.py
+++ tests/test_inventory.py
@@ -4,7 +4,10 @@
 """ Test Artifacts """
 
 
-from os.path import dirname, relpath, basename
+from os import makedirs, utime
+from os.path import dirname, relpath, basename, join
+from tempfile import TemporaryDirectory
+from time import time
 
 from genweb.inventory import Artifacts
 
@@ -61,6 +64,60 @@ def test_basic() -> None:
     assert "data/test.xml" in artifacts.files_under("data")
 
 
+def create_file(path: str, contents: str):
+    makedirs(dirname(path), exist_ok=True)
+    with open(path, "w", encoding="utf-8") as file:
+        file.write(contents)
+
+
+def test_add() -> None:
+    with TemporaryDirectory() as working_dir:
+        create_file(join(working_dir, "file1.txt"), "file1")
+        create_file(join(working_dir, "file2.txt"), "file2")
+        create_file(join(working_dir, "dir/file3.txt"), "file3")
+        artifacts = Artifacts(working_dir)
+        create_file(join(working_dir, "dir/file4.txt"), "file4")
+        artifacts.add("dir/file4.txt")
+        artifacts.add("file1.txt")
+
+        try:
+            artifacts.add("dir/file5.txt")
+            raise AssertionError("dir/file5.txt should have failed")
+
+        except AssertionError:
+            pass
+
+        lost = {"file2.txt", "dir/file3.txt"}
+        assert set(artifacts.lost()) == lost, artifacts.lost()
+
+
+def test_hash() -> None:
+    with TemporaryDirectory() as working_dir:
+        create_file(join(working_dir, "file1.txt"), "")
+        create_file(join(working_dir, "dir/file2.txt"), "2")
+        artifacts = Artifacts(working_dir)
+        hash_empty = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+        hash_2 = "d4735e3a265e16eee03f59718b9b5d03019c07d8b6c51f90da3a666eec13ab35"
+        assert artifacts.hash("file1.txt") == hash_empty, artifacts.hash("file1.txt")
+        assert artifacts.hash("dir/file2.txt") == hash_2, artifacts.hash(
+            "dir/file2.txt"
+        )
+        create_file(join(working_dir, "dir/file2.txt"), "2 ")
+        hash_2_space = (
+            "5749fdd6b67e4204b3047ba33540bc87f60c84d784a46c6307c78299f8fa67e9"
+        )
+        artifacts = Artifacts(working_dir)
+        assert artifacts.hash("file1.txt") == hash_empty, artifacts.hash("file1.txt")
+        assert artifacts.hash("dir/file2.txt") == hash_2_space, artifacts.hash(
+            "dir/file2.txt"
+        )
+        minute_ago = time() - 60
+        utime(join(working_dir, "file1.txt"), (minute_ago, minute_ago))
+        assert artifacts.hash("file1.txt") == hash_empty, artifacts.hash("file1.txt")
+
+
 if __name__ == "__main__":
     test_basic()
     test_suffixed()
+    test_add()
+    test_hash()
